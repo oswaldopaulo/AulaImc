162e374c6202438277ea8f889da7bb15567f108e
Oswaldo Paulo <oswaldo.paulo@gmail.com>
2021-03-17 21:14:43 -0300

    fun calcular

--- app/src/main/java/com/oswaldopaulo/imc/MainActivity.kt
+++ app/src/main/java/com/oswaldopaulo/imc/MainActivity.kt
@@ -3,6 +3,10 @@ package com.oswaldopaulo.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.pow
+import kotlin.math.round
+import kotlin.math.roundToInt
+import kotlin.math.roundToLong
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +18,19 @@ class MainActivity : AppCompatActivity() {
     private fun setListeners(){
 
         calcularBTN.setOnClickListener{
+            val altura = alturaEDT.text.toString().toDouble()
+            val peso  = pesoEDT.text.toString().toDouble()
 
+            titleTXT.setText( imc(peso, altura).toString() )
         }
+    }
+
+    fun imc(peso: Double, altura :Double) : Double{
+        fun Double.format(digits: Int) = "%.${digits}f".format(this).replace(',','.').toDouble()
+
+        if(altura <= 0) return 0.00
+        if(peso <= 0) return 0.00
+        val res = peso / (altura).pow(2)
+        return res.format(2)
     }
 }
